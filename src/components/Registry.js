5ecd79ff4ebdf290a840f9ac59fcbe8b2bd86983
Mike Reinhard <mike@mikereinhard.com>
2019-05-26 01:47:32 -0400

    align amazon and crate&barrel logo

--- src/components/Registry.js
+++ src/components/Registry.js
@@ -10,13 +10,13 @@ class Registry extends PureComponent {
       <section className='registry'>
         <h2 className='dark'>We're registered at...</h2>
         <div className='container'>
-          <a className='registery-item' href='https://www.amazon.com/wedding/mike-reinhard-hannah-stoker-orlando-august-2019/registry/260B2VBVY89D7'>
+          <a className='registery-item amazon' href='https://www.amazon.com/wedding/mike-reinhard-hannah-stoker-orlando-august-2019/registry/260B2VBVY89D7'>
             <img src={amazon} alt='amazon.com' />
           </a>
-          <a className='registery-item' href='https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/547286125?eventType=Wedding'>
+          <a className='registery-item bbb' href='https://www.bedbathandbeyond.com/store/giftregistry/viewregistryguest/547286125?eventType=Wedding'>
             <img src={bedbathandbeyond} alt='bedbathbeyond.com' />
           </a>
-          <a className='registery-item' href='https://www.crateandbarrel.com/gift-registry/hannah-stoker-and-mike-reinhard/r5963049'>
+          <a className='registery-item cb' href='https://www.crateandbarrel.com/gift-registry/hannah-stoker-and-mike-reinhard/r5963049'>
             <img src={crateandbarrel} alt='crateandbarrel.com' />
           </a>
         </div>
